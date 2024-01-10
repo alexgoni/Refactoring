937995ecbe39f3e7be52aa8d2d8a17c287079597
alexgoni <goni000211@gmail.com>
2024-01-10 18:45:03 +0900

    refactor: divide statement to createStatementData

---
+++ CH 01/statement.js
@@ -0,0 +1,25 @@
+import createStatementData from "./createStatementData.js";
+
+export function statement(invoice, plays) {
+  return renderPlaneText(createStatementData(invoice, plays));
+}
+
+function renderPlaneText(data) {
+  let result = `청구내역 (고객명: ${data.customer})\n`;
+
+  for (let perf of data.performances) {
+    result += `${perf.play.name}: ${usd(perf.amount)} ${perf.audience}석\n`;
+  }
+
+  result += `총액 ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트 ${data.totalVolumeCredits}점\n`;
+  return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    maximumFractionDigits: 2,
+  }).format(aNumber / 100);
+}
